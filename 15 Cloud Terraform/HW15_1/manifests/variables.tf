e91c4d2240131aaa7a4d6b58af86e41a08b1d34c
Alex-Elfman <118746742+Alex-Elfman@users.noreply.github.com>
2024-04-04 21:04:52 +0500

    HW15_1 Cloud_Terraform

---
+++ 15 Cloud Terraform/HW15_1/manifests/variables.tf
@@ -0,0 +1,39 @@
+variable "yandex_cloud_id" {
+  default = "b1g9ad2qr4nsh3ege1ml"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g7vp08e8h6sbibbq9h"
+}
+
+variable "a-zone" {
+  default = "ru-central1-a"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd8dmsb2cgoabg4qelih"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "ubuntu" {
+  default = "fd8dmsb2cgoabg4qelih"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
